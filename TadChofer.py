9a174e77f05056cff08ec0d248eaa13ac97765d0
vluend <vluendy@gmail.com>
2025-05-21 16:59:11 -0300

    Agrege lo mas basico y algunos tads.. todavia me falta completar, y lo de losd emas tad tambien, estaria bueno que empecemos a hacer la app y ver que podemos incluir en app que la implementacion  no deberia

---
+++ TadChofer.py
@@ -0,0 +1,38 @@
+
+
+def CrearChofer():
+    chofer = ['','',0,0,0,0]
+#chofer[0]:nombre,
+#chofer[1]: apellido,
+#chofer[2]: legajo,
+#chofer[3] fecha de ingreso,
+#chofer[4]: número de camión
+#chofer[5]: zona de trabajo.
+
+def CargarChofer(ListaChofer, n, a, l, f, c, z):
+    ListaChofer[0] = n
+    #.....
+#bueno etc esto lo hace axel
+
+
+
+
+def ModificarNom(chofer, n):
+    chofer[0] = n
+
+def ModificarApe(chofer, a):
+    chofer[1] = a
+
+def MoficarLeg(chofer, l):
+    chofer[2] = l
+
+def ModificarFecha(chofer,f):
+    chofer[3] = f
+
+def ModificarNroCamion(chofer,c):
+    chofer[4] = c
+
+def ModificarZonaTrabajo(chofer, z):
+    chofer[5] = z 
+
+
---
+++ TadCola.py
@@ -0,0 +1,88 @@
+## Enunciado 8
+'''
+Desarrollar una aplicación que permita gestionar la información de los choferes y sus camiones en
+una empresa de transporte de carga, utilizando los Tipos Abstractos de Datos (TADs) que el
+estudiante considere adecuados.
+
+De cada chofer se registran: nombre, apellido, legajo, fecha de ingreso, número de camión y zona de trabajo.
+
+El sistema debe ofrecer un **menú interactivo** que permita realizar las operaciones detalladas a continuación.
+
+a. Alta y modificación de choferes:
+Implementar funciones para agregar nuevos choferes con todos sus datos, así como modificar la
+información de un chofer existente (nombre, zona de trabajo, número de camión, etc.).
+
+b. Eliminación segura de choferes:
+Permitir eliminar choferes del sistema utilizando su legajo como identificador único, asegurando que
+los datos se eliminen correctamente.
+
+c. Visualización completa:
+Generar y mostrar un listado con todos los choferes registrados, incluyendo todos sus datos.
+
+d. Cambio masivo de zona por año de ingreso:
+Permitir cambiar la zona de trabajo de todos los choferes que ingresaron en un año determinado,
+reorganizando así la asignación geográfica.
+
+e. Depuración por antigüedad:
+Eliminar automáticamente del sistema a los choferes cuya antigüedad supere los 30 años, calculada
+a partir de su fecha de ingreso.
+
+f. Cola por zona de trabajo:
+Generar una cola con los choferes de una zona específica, y mostrar en pantalla los datos de cada
+uno en el orden correspondiente.
+
+Muchachos entonces el menu se hace en la aplicacion donde integramos todos los tad que vamos hacer
+
+Los TADS:
+TAD SIMPLE DE CHOFER
+    (CREAR, CARGAR, VER, MODIFICAR, ASIGNAR O COPIAR)
+
+TAD COMPUESTO TIPO LISTA DE CHOFERES
+    (CREAR, AGREGAR, ELIMINAR, RECUPERAR, TAMANIO)
+    
+
+    (CREAR, ENCOLAR, DESENCOLAR, TAMANIO, ESVACIA)
+
+'''
+
+def CrearListaChofer():
+    ListaChofer = []
+    return ListaChofer
+
+def AgregarChofer(ListaChofer, nuevo):
+    ListaChofer.append(nuevo)
+
+def EliminarChofer(ListaChofer, chofer):
+    ListaChofer.remove(chofer)
+#pero en este nos piden que sea por legajo... entonces habria que hacer alguna modificacion desde la aplicacion.??
+
+def RecuperarChofer(ListaChofer, i):
+    return ListaChofer[i-1]
+
+def TamanioListaChofer(ListaChofer):
+    return len(ListaChofer)
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
